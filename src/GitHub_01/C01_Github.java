fe7534d42298c20bfcb0b2769e9cc4a41d3555c9
colak <colakogun@yahoo.com>
2023-02-02 19:53:11 -0800

    simpleman added 2 lines of code

--- src/GitHub_01/C01_Github.java
+++ src/GitHub_01/C01_Github.java
@@ -1,5 +1,11 @@
 package GitHub_01;
 
 public class C01_Github {
+    public static void main(String[] args) {
+        System.out.println("All good");
+        System.out.println("Learning GitHub");
+
+
+    }
 
 }
