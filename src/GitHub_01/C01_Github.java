5e2a253a5c8841ba9b5608ad731f095e1d96edc4
Melisa Canbaz <melisa.canbazz@gmail.com>
2023-02-02 22:51:19 -0500

    2 satir kod ekledim

--- src/GitHub_01/C01_Github.java
+++ src/GitHub_01/C01_Github.java
@@ -1,5 +1,11 @@
 package GitHub_01;
 
 public class C01_Github {
+    public static void main(String[] args) {
+        System.out.println("1. satir kod");
+        System.out.println("Hello world");
+
+
+    }
 
 }
